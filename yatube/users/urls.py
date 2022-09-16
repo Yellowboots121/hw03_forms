b34da0a88c117418173ceb54b8fb5be6e53f7c47
Михаил Попов <yellowboots6@yandex.ru>
2022-09-16 15:36:32 +0300

    4 спринт вер 2

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -19,32 +19,44 @@ urlpatterns = [
     ),
     path(
         'password_change/',
-        aw.PasswordChangeView.as_view(template_name='users/password_change_form.html'),
-        name='password_change'
+        aw.PasswordChangeView.as_view(
+            template_name='users/password_change_form.html'
         ),
+        name='password_change'
+    ),
     path(
         'password_change/done/',
-        aw.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+        aw.PasswordChangeDoneView.as_view(
+            template_name='users/password_change_done.html'
+        ),
         name='password_change_done'
     ),
     path(
         'password_reset/',
-        aw.PasswordResetView.as_view(template_name='users/password_reset_form.html'),
+        aw.PasswordResetView.as_view(
+            template_name='users/password_reset_form.html'
+        ),
         name='password_reset'
     ),
     path(
         'password_reset/done/',
-        aw.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+        aw.PasswordResetDoneView.as_view(
+            template_name='users/password_reset_done.html'
+        ),
         name='password_reset_done'
     ),
     path(
         'reset/<uidb64>/<token>/',
-        aw.PasswordResetConfirmView.as_view(template_name='users/password_reset_confim.html'),
+        aw.PasswordResetConfirmView.as_view(
+            template_name='users/password_reset_confim.html'
+        ),
         name='password_reset_confim'
     ),
     path(
         'reset/done/',
-        aw.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+        aw.PasswordResetCompleteView.as_view(
+            template_name='users/password_reset_complete.html'
+        ),
         name='password_reset_complete'
     ),
 ]
